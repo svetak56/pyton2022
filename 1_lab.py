f8d187c53f538c1d3563dbd79a456ec9b6ca9105
VaH31 <wh8ox@yandex.ru>
2021-10-16 12:20:49 +0300

    [1] Holmov
    Test

--- 1_lab.py
+++ 1_lab.py
@@ -1,1 +1,12 @@
-1
+mixed_list = [3, .42345, 'eras', (3+5j), '173', 65535, -391.32371623346, None]
+
+filtered_list = [x for x in mixed_list if x!=None]
+
+turple_list = (3, 1, .032, 'ryry', -3221.3321, None, None]
+
+dict_list = {'int': 16768, 'float': -1.3273, 'str':'foo_cad', 'complex':(5+1.3j), 'ept':None}
+
+filt_dict = {key: value for key, value in dict_list.items() if len(key)>3 or value!=None}
+
+print(filt_dict)
+print(filtered_list)
